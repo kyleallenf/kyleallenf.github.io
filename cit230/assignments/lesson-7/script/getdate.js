39700b244d7a8e10d6d3da686205023a99bdbc45
kyleallenf <kylefoutz777@gmail.com>
2018-06-06 09:44:01 -0600

    working

---
+++ cit230/assignments/lesson-7/script/getdate.js
@@ -0,0 +1,14 @@
+/*var currentdate = new Date();
+
+var daysOfWeek = new Array(7);
+daysOfWeek[0] = "Sunday";
+daysOfWeek[1] = "Monday";
+daysOfWeek[2] = "Tuesday";
+daysOfWeek[3] = "Wednesday";
+daysOfWeek[4] = "Thirsday";
+daysOfWeek[5] = "Friday";
+daysOfWeek[6] = "Saturday";
+
+var dayOfWeekNumber = currentdate.getDate();
+var dayOfWeekString = daysOfWeek[dayOfWeekNumber] + ", ";
+*/
